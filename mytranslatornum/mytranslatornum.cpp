8e3d83c9800e0a94f51cb40694cebe3aa3f6b5d8
AnDreV133 <dmitrijkubik@gmail.com>
2023-10-14 17:18:54 +0300

    add test n function

--- mytranslatornum/mytranslatornum.cpp
+++ mytranslatornum/mytranslatornum.cpp
@@ -2,5 +2,110 @@
 // Created by dmitr on 14.10.2023.
 //
 
+#include <cmath>
+#include <cstdio>
 #include "mytranslatornum.h"
 
+long long getIntFromBinInSimplePresentation(const char *begin, const char *end) {
+    int sign = (*begin == '-') ? -1 : 1;
+    if (sign < 0) begin++;
+    long long term = 1;
+    long long val = 0;
+    while (begin < end) {
+        end--;
+        if (*end == '1')
+            val += term;
+        term <<= 1;
+    }
+    return sign * val;
+}
+double getFloatFromBinInSimplePresentation(const char *begin, const char *end) {
+    int sign = (*begin == '-') ? -1 : 1;
+    if (sign < 0) begin++;
+    const char *splitPoint = begin;
+    while (splitPoint < end && *splitPoint != '.')
+        splitPoint++;
+    long long intPart = getIntFromBinInSimplePresentation(begin, splitPoint);
+    double term = 0.5;
+    double val = 0;
+    while (splitPoint < end) {
+        splitPoint++;
+        if (*splitPoint == '1')
+            val += term;
+        term /= 2;
+    }
+    return ((double) intPart + val) * sign;
+}
+double getFloatFromBin(const char *begin, const char *end) {
+    int sign = (*begin == '1') ? -1 : 1;
+
+    long long shift = getIntFromBinInSimplePresentation(begin + 1, begin + 9) - 127;
+    double term = pow(2, shift);
+    double val = term;
+    begin += 9;
+    while (begin < end) {
+        term /= 2;
+        if (*begin == '1')
+            val += term;
+        begin++;
+    }
+    return ((double) val) * sign;
+}
+long long getIntFromBin(const char *begin, const char *end) {
+    long long term = 1;
+    long long val = 0;
+    if (*begin == '0') {
+        while (begin < end) {
+            end--;
+            if (*end == '1')
+                val += term;
+            term <<= 1;
+        }
+        return val;
+    } else {
+        while (begin < end) {
+            end--;
+            if (*end == '0')
+                val += term;
+            term <<= 1;
+        }
+        return -val - 1;
+    }
+}
+
+bool getBit(long long a, long long i) {
+    return a & (1 << i);
+}
+void printByte(long int a) {
+    for (long long i = sizeof(long int) * 8 - 1; i >= 0; --i)
+        printf("%d", getBit(a, i));
+    printf("\n");
+}
+void printByte(float a) {
+    auto copyA = reinterpret_cast<long *>(&a);
+    for (long long i = sizeof(float) * 8 - 1; i >= 0; --i)
+        printf("%d", getBit(*copyA, i));
+    printf("\n");
+}
+void printByte(char *a) {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            for (long long k = sizeof(char) * 8 - 1; k >= 0; --k)
+                printf("%d", getBit((long long) a[3*i + j], k));
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
+void printVar(long int *a, unsigned size) {
+    for (int i = 0; i < size; ++i)
+        printByte(a[i]);
+}
+void printVar(float *a, unsigned size) {
+    for (int i = 0; i < size; ++i)
+        printByte(a[i]);
+}
+void printVar(char **a, unsigned size) {
+    for (int i = 0; i < size; ++i)
+        printByte(a[i]);
+}
--- tests/test_mytranslatornum.cpp
+++ tests/test_mytranslatornum.cpp
@@ -2,6 +2,7 @@
 // Created by dmitr on 14.10.2023.
 //
 
+#include <iostream>
 #include "test_mytranslatornum.h"
 #include "../mytranslatornum/mytranslatornum.h"
 
@@ -21,4 +22,17 @@ void test_outputInBinFormat() {
 
     long int lia[] = {4, 2};
     printVar(lia, 2);
+}
+
+void test_outputInDigitFromBinFormat() {
+    char s1[] = "-101010";
+    std::cout << getIntFromBinInSimplePresentation(s1, s1 + 7) << '\n';
+    char s2[] = "101010.011010111000010101";
+    std::cout << getFloatFromBinInSimplePresentation(s2, s2 + 25) << '\n';
+    char s3[] = "00000000000000000000000000101010";
+    std::cout << getIntFromBin(s3, s3 + 32) << '\n';
+    char s3_[] = "11111111111111111111111111010110";
+    std::cout << getIntFromBin(s3_, s3_ + 32) << '\n';
+    char s4[] = "01000010001010011010111000010101";
+    std::cout << getFloatFromBin(s4, s4 + 32) << '\n';
 }
