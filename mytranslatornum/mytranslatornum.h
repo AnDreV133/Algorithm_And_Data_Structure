fcd7452a3ab35f6f149d1cfe76f0bcc8cbddd40e
AnDreV133 <dmitrijkubik@gmail.com>
2023-10-14 17:18:54 +0300

    add test n function

--- mytranslatornum/mytranslatornum.h
+++ mytranslatornum/mytranslatornum.h
@@ -6,10 +6,23 @@
 #define PROJECT_MYTRANSLATORNUM_H
 
 void printByte(long int a);
+
 void printByte(float a);
+
 void printByte(char *a);
+
 void printVar(long int *a, unsigned size);
+
 void printVar(float *a, unsigned size);
+
 void printVar(char **a, unsigned size);
+
+long long getIntFromBin(const char *begin, const char *end);
+
+double getFloatFromBin(const char *begin, const char *end);
+
+double getFloatFromBinInSimplePresentation(const char *begin, const char *end);
+
+long long getIntFromBinInSimplePresentation(const char *begin, const char *end;
 
 #endif //PROJECT_MYTRANSLATORNUM_H
