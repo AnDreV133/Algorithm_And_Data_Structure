1e8e9b3583c607b1d2b2c258e2d3c2b2f1308182
AnDreV133 <dmitrijkubik@gmail.com>
2023-10-14 17:44:34 +0300

    add headers

--- mystring/mystring.h
+++ mystring/mystring.h
@@ -5,4 +5,43 @@
 #ifndef PROJECT_MYSTRING_H
 #define PROJECT_MYSTRING_H
 
+enum ExceptionalSituations {
+    NO_ERROR = 0,
+    MEMORY_ALLOCATION_ERROR = 1,
+    STRING_TOO_LONG_ERROR = 2,
+    INDEX_OUT_OF_BOUNDS_ERROR = 3,
+    INVALID_STRING_ERROR = 4
+};
+extern int StrError; // Переменная ошибок
+typedef struct String {
+    char *s; // Признак конца строки - символ '\0'
+    unsigned max; // Макс кол-во символов в строке, определяется при инициализации
+} String;
+
+void InitStr(String *st, unsigned n);
+
+void WriteToStr(String *st, char *s);
+
+void WriteFromStr(char *s, String *st);
+
+void InputStr(String *st);
+
+void OutputStr(String *st);
+
+int Comp(String *s1, String *s2);
+
+void Delete(String *s, unsigned Index, unsigned Count);
+
+void Insert(String *Subs, String *s, unsigned Index);
+
+void Concat(String *s1, String *s2, String *srez);
+
+void Copy(String *s, unsigned Index, unsigned Count, String *Subs);
+
+unsigned Length(String *s);
+
+unsigned Pos(String *SubS, String *s);
+
+void DoneStr(String *s);
+
 #endif //PROJECT_MYSTRING_H
