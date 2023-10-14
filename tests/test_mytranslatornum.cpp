3dd9a5a06360bac561d7a9799587570b750bece4
AnDreV133 <dmitrijkubik@gmail.com>
2023-10-14 16:42:29 +0300

    add test

--- tests/test_mytranslatornum.cpp
+++ tests/test_mytranslatornum.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "test_mytranslatornum.h"
+#include "../mytranslatornum/mytranslatornum.h"
+
+void test_outputInBinFormat() {
+    long int li = 42;
+    printByte(li);
+
+    float f = 42.42;
+    printByte(f);
+
+    char cm[][3] = {
+            4, 2, 42,
+            40, 20, 24,
+            0, 1, 10
+    };
+    printByte((char *) cm);
+
+    long int lia[] = {4, 2};
+    printVar(lia, 2);
+}
