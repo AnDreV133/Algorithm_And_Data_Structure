18fe68c1cf0d8a62ac8156a2177e9c0aeb57737e
AnDreV133 <dmitrijkubik@gmail.com>
2023-10-14 16:36:36 +0300

    add function

--- mytranslatornum/mytranslatornum.h
+++ mytranslatornum/mytranslatornum.h
@@ -5,4 +5,11 @@
 #ifndef PROJECT_MYTRANSLATORNUM_H
 #define PROJECT_MYTRANSLATORNUM_H
 
+void printByte(long int a);
+void printByte(float a);
+void printByte(char *a);
+void printVar(long int *a, unsigned size);
+void printVar(float *a, unsigned size);
+void printVar(char **a, unsigned size);
+
 #endif //PROJECT_MYTRANSLATORNUM_H
