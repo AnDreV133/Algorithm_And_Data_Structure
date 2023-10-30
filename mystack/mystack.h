19858be0318f7fda1718d084fafcb57872fd5160
AnDreV133 <88576180+AnDreV133@users.noreply.github.com>
2023-10-30 12:55:57 +0300

    stack

--- mystack/mystack.h
+++ mystack/mystack.h
@@ -5,6 +5,38 @@
 #ifndef PROJECT_MYSTACK_H
 #define PROJECT_MYSTACK_H
 
+const int STACK_SIZE = 1000;
 
+int StackError;
 
-#endif //PROJECT_MYSTACK_H
+enum ExceptionalSituations
+{
+    StackOk,
+    StackNotMem,
+    StackEnd,
+    StackOver,
+    StackUnder
+};
+
+typedef int BaseType; // Определить тип элемента стека
+
+typedef struct Stack
+{
+    BaseType buf[STACK_SIZE];
+    unsigned uk; /* Указывает на элемент, являющийся вершиной
+    стека */
+} Stack;
+
+void initStackArray(Stack *S);
+
+void PutStack(Stack *S, BaseType E);
+
+void GetStack(Stack *S, BaseType *E);
+
+int isFullStackArray(Stack *S);
+
+int isEmptyStackArray(Stack *S);
+
+void ReadStack(Stack *s, BaseType *E);
+
+#endif // PROJECT_MYSTACK_H
